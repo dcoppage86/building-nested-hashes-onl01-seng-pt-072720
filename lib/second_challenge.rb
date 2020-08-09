abc8f4d8536b708b6a650f2500a1bf074996b098
Dakota Coppage <dakotakcoppage@me.com>
2020-08-09 22:05:11 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,6 @@
 def second_challenge
-  epic_tragedy = {
-   :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-   },
-   :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-   }
-    
-  }
+epic_tragedy = {
+  :montague => {}
+  :capulet => {}
+}
 end
