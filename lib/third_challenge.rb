eae6a0d7f9dd38d8c6df95be64a4f72179c157fa
Dakota Coppage <dakotakcoppage@me.com>
2020-08-09 22:24:50 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,30 +2,31 @@ def third_challenge(epic_tragedy)
   epic_tragedy = {
   :montague => {
     :patriarch => {  
-      :name => "Lord Montague",
+      :name => "Lord Montague", 
       :age => "53"
-  },
-    :matriarch => {
-      :name => "Lady Montague",
+  }, 
+  :matriarch => {
+      :name => "Lady Montague", 
       :age => "54"
-  },
-    :hero => {
-      :name => "Romeo",
+  }, 
+  :hero => {
+      :name => "Romeo", 
       :age => "15"
-  },
-    :hero_friends => []
+  }, 
+  :hero_friends => []
   },
   :capulet => {
     :patriarch => {
-      :name => "Lord Capulet",
+      :name => "Lord Capulet", 
       :age => "50"
-    },
+    }, 
     :matriarch => {
-      :name => "Lady Capulet",
+      :name => "Lady Capulet", 
       :age => "51"
-    },
+    }, 
     :heroine => {
-      :name => "Juliet", :age => "15",
+      :name => "Juliet", 
+      :age => "15", 
       :status => "alive"
     },
     :heroine_friends=> []
