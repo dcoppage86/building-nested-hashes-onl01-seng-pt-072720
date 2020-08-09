37acc6afdbedf8a3137d937ee7faf08bad4ea8c7
Dakota Coppage <dakotakcoppage@me.com>
2020-08-09 22:26:36 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,4 +5,18 @@ def third_challenge
       :name => "Lord Montague",
       :age => "53"
   }, 
+      :matriarch => {
+      :name => "Lady Montague",
+      :age => "54"
+  },
+    :hero => {
+      :name => "Romeo",
+      :age => "15"
+  },
+    :hero_friends => [
+      {
+         :name => "Benvolio",
+         :age => "17",
+         :attitude => "worried"
+      },
   
