bec5d8e2f2eab91cd8998103fb9a179d1b003741
Dakota Coppage <dakotakcoppage@me.com>
2020-08-09 22:38:40 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,8 +4,8 @@ def third_challenge
     :patriarch => {  
       :name => "Lord Montague",
       :age => "53"
-  }, 
-      :matriarch => {
+  },
+    :matriarch => {
       :name => "Lady Montague",
       :age => "54"
   },
@@ -13,24 +13,23 @@ def third_challenge
       :name => "Romeo",
       :age => "15"
   },
-    :hero_friends => [
-      {
-         :name => "Benvolio",
-         :age => "17",
-         :attitude => "worried"
-      }, 
-      {
-        :name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"
-      }
-    ]
-  }, 
-        {
-        :name => "Mercutio",
-        :age => "18",
-        :attitude => "hot-headed"
-      }
-    ]
+    :hero_friends => []
   },
+  :capulet => {
+    :patriarch => {
+      :name => "Lord Capulet",
+      :age => "50"
+    },
+    :matriarch => {
+      :name => "Lady Capulet",
+      :age => "51"
+    },
+    :heroine => {
+      :name => "Juliet",
+      :age => "15",
+      :status => "alive"
+    },
+    :heroine_friends=> []
+  }
+}
 end
